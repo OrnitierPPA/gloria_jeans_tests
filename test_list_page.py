3b595bb2e6e4ccc51ddaee05411b679726b6fbe1
Arkady Popov <associatedtolife@mail.ru>
2023-02-08 14:37:04 +0400

    Add marks

--- test_list_page.py
+++ test_list_page.py
@@ -1,6 +1,8 @@
+import pytest
+
 from pages.list_page import ListPage
 
-
+@pytest.mark.basket_tests
 def test_add_to_basket_from_product_list(browser):
     link = "https://www.gloria-jeans.ru/c/girls-jeans"
     page = ListPage(browser, link)
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,9 @@
+import pytest
+
 from pages.product_page import ProductPage
 
 
+@pytest.mark.basket_tests
 def test_add_to_basket_from_product_page(browser):
     link = "https://www.gloria-jeans.ru/p/GSW005760-3/Molocnyj-ukorocennyj-kardigan-s-ob%22emnymi-rukavami-dla-devoc"
     page = ProductPage(browser, link)
