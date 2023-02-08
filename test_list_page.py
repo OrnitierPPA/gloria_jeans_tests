2a2afee84f3c9398c158dfc3c6e7c6f15e7ac5df
Arkady Popov <associatedtolife@mail.ru>
2023-02-08 12:13:30 +0400

    Add tests for adding a product to the basket from the list page and product page

---
+++ test_list_page.py
@@ -0,0 +1,9 @@
+from pages.list_page import ListPage
+
+
+def test_add_to_basket_from_product_list(browser):
+    link = "https://www.gloria-jeans.ru/c/girls-jeans"
+    page = ListPage(browser, link)
+    page.open()
+    page.city_notification()
+    page.add_product_in_basket()
---
+++ test_product_page.py
@@ -0,0 +1,10 @@
+from pages.product_page import ProductPage
+
+
+def test_add_to_basket_from_product_page(browser):
+    link = "https://www.gloria-jeans.ru/p/GSW005760-3/Molocnyj-ukorocennyj-kardigan-s-ob%22emnymi-rukavami-dla-devoc"
+    page = ProductPage(browser, link)
+    page.open()
+    page.city_notification()
+    page.add_product_in_basket()
+
